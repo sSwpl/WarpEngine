4e183d11e09aea2f3802834af278cd3d06a34590
sSwpl <73335669+sSwpl@users.noreply.github.com>
2026-02-10 00:31:20 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Tu zaczniemy pisać kod
+// To jest główny plik wejściowy silnika
+
+int main() {
+    std::cout << "Warp Engine Initialized" << std::endl;
+    // Inicjalizacja bibliotek (GLFW, WebGPU)
+    // Pętla główna gry
+    return 0;
+}
